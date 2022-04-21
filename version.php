eb81595a13cae4264d05b9d3bf3b93eabfe7ba82
Matt Davidson <davidso1@rose-hulman.edu>
2022-04-21 16:11:33 -0400

    add onetopic format

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2022102300;
+$plugin->version = 2023041700;
 $plugin->requires  = 2022041900;
 $plugin->component = 'block_massaction';
 $plugin->maturity = MATURITY_STABLE;
